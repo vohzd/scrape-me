593b54d2443818b47261976f1dbbee08dafdf852
Ben <allobon@gmail.com>
2019-06-27 16:33:51 +0100

    more

--- routes/scrape/index.js
+++ routes/scrape/index.js
@@ -6,7 +6,7 @@ const puppeteer                           = require("puppeteer");
 
 router.get("/", async (req, res, next) => {
 
-  await browser = puppeteer.launch();
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.boohoo.com/");
   let bodyHTML = await page.evaluate(() => document.body.innerHTML);
