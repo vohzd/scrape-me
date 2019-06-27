e3b706bf5575d44ffe345474af426645165350c6
Ben <allobon@gmail.com>
2019-06-27 16:32:52 +0100

    fix bug

--- routes/scrape/index.js
+++ routes/scrape/index.js
@@ -5,24 +5,14 @@ const router                              = express.Router();
 const puppeteer                           = require("puppeteer");
 
 router.get("/", async (req, res, next) => {
-  await browser = puppeteer.launch({
-    args: ["--disable-dev-shm-usage"],
-    executablePath: "/usr/bin/chromium-browser"
-  });
-  puppeteer.launch().then(async browser => {
-      const page = await browser.newPage();
-      await page.goto("https://www.boohoo.com/");
 
-      /*
-      const productName = await page.$('.product-name');
-      const text = await (await productName.getProperty('textContent')).jsonValue();
-      console.log(text);*/
+  await browser = puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.boohoo.com/");
+  let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+  console.log(bodyHTML);
+  return res.send({ "html": bodyHTML});
 
-      let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-      console.log(bodyHTML);
-
-      return res.send({ "html": bodyHTML});
-  });
 });
 
 
