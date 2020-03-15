e7a4374d01fadfab81dc1acc04458a7140550f5f
Ben <allobon@gmail.com>
2020-03-15 18:13:00 +0000

    return images

--- routes/scrape/index.js
+++ routes/scrape/index.js
@@ -5,15 +5,27 @@ const router                              = express.Router();
 const puppeteer                           = require("puppeteer");
 
 router.get("/", async (req, res, next) => {
-
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.boohoo.com/");
-  let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-  console.log(bodyHTML);
-  return res.send({ "html": bodyHTML});
+  return res.send("server booted");
 
 });
 
+
+
+
+router.get("/website", async (req, res, next) => {
+  // call it like this example.com/website?url=https://YOURWEBSITE.com
+  const url = req.query.url;
+  console.log(req.query)
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  console.log("GOING")
+  const images = await page.evaluate( () => Array.from( document.images, e => e.src ) );
+  console.log(images);
+  /* for all html
+  let bodyHTML = await page.evaluate(() => document.querySelector("img"));*/
+  return res.send({ "images": images });
+
+});
 
 module.exports = router;
