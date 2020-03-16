fc6c0afa123a518af5709734b05fc45b074e0b35
Ben <allobon@gmail.com>
2020-03-16 17:43:07 +0000

    stealth plugin

--- routes/scrape/index.js
+++ routes/scrape/index.js
@@ -2,7 +2,10 @@ const express                             = require("express");
 
 const router                              = express.Router();
 
-const puppeteer                           = require("puppeteer");
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+console.log(StealthPlugin)
+puppeteer.use(StealthPlugin())
 
 router.get("/", async (req, res, next) => {
   return res.send("server booted");
@@ -16,15 +19,25 @@ router.get("/website", async (req, res, next) => {
   // call it like this example.com/website?url=https://YOURWEBSITE.com
   const url = req.query.url;
   console.log(req.query)
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
+    await page.waitFor(1000)
   console.log("GOING")
-  const images = await page.evaluate( () => Array.from( document.images, e => e.src ) );
-  console.log(images);
+
+
+  try {
+    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    //const images = await page.evaluate( () => Array.from( document.images, e => e.src ) );
+    return res.send({ "html": bodyHTML });
+
+  }
+  catch (e){
+    return res.send({ "res": "fail" });
+
+  }
   /* for all html
   let bodyHTML = await page.evaluate(() => document.querySelector("img"));*/
-  return res.send({ "images": images });
 
 });
 
