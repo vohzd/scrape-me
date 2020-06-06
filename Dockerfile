e0b5cf36f7ef34d81e7bef5aaa2567b7c6f3e225
Ben <allobon@gmail.com>
2020-06-06 11:26:33 +0100

    think versions are all broken

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mhart/alpine-node:12
+FROM mhart/alpine-node:10
 
 # Installs latest Chromium (77) package.
 RUN apk add --no-cache \
--- routes/scrape/index.js
+++ routes/scrape/index.js
@@ -18,7 +18,10 @@ router.get("/website", async (req, res, next) => {
   // call it like this example.com/website?url=https://YOURWEBSITE.com
   const url = req.query.url;
   console.log(req.query)
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--disable-dev-shm-usage']
+  });
   const page = await browser.newPage();
   await page.goto(url);
   await page.waitFor(1000)
